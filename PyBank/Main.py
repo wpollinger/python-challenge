f7b0653ab03c85a65406d016444b554fa958a6a9
wpollinger <wendy_tion@yahoo.com>
2018-11-15 17:40:59 -0800

    This is updated

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,33 +1,64 @@
-# Modules
 import os
 import csv
 
-# Prompt user for video lookup
-video = input("What show or movie are you looking for? ")
 
 # Set path for file
-csvpath = os.path.join("..", "Resources", "netflix_ratings.csv")
+csvpath = os.path.join(".", "Resources", "budget_data.csv")
+# Create empty lists to iterate through specific rows for the following variables
+total_months = []
+total_profit = []
+monthly_profit_change = []
 
-# Bonus
-# ------------------------------------------
-# Set variable to check if we found the video
-found = False
+# Open csv in default read mode with context manager
+with open(csvpath) as budget:
 
-# Open the CSV
-with open(csvpath, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-
-    # Loop through looking for the video
+     # Store the contents of budget_data.csv in the variable csvreader
+    csvreader = csv.DictReader(budget,delimiter=",")
+    # Skip the header labels to iterate with the values
+    #header = next(csvreader)
+    # Iterate through the rows in the stored file contents
     for row in csvreader:
-        if row[0].lower().replace(' ', '') == video.lower().replace(' ', ''):
-            print(row[0] + " is rated " + row[1] + " with a rating of " + row[5])
+    # Append the total months and total profit to their corresponding lists
+        total_months.append(row['Date'])
+        total_profit.append(int(row['Profit/Losses']))
+    # Iterate through the profits in order to get the monthly change in profits
+    for i in range(len(total_profit)-1):
+    # Take the difference between two months and append to monthly profit change
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+# Obtain the max and min of the the montly profit change list
+max_increase_value = max(monthly_profit_change)
+max_decrease_value = min(monthly_profit_change)
 
-            # BONUS: Set variable to confirm we have found the video
-            found = True
+# Correlate max and min to the proper month using month list and index from max and min
+#We use the plus 1 at the end since month associated with change is the + 1 month or next month
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1
 
-            # BONUS: Stop at first results to avoid duplicates
-            break
+# Print Statements
 
-    # If the video is never found, alert the user
-    if not found:
-        print("Sorry about this, we don't seem to have what you are looking for!")
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
+# Output files
+output_file = os.path.join(".", "Resources", "output.txt")
+with open(output_file,"w") as file:
+
+# Write into the output.txt file
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(total_months)}")
+    file.write("\n")
+    file.write(f"Total: ${sum(total_profit)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
